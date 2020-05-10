d41870faf69663ff16741328eecd91859fe78be6
masakih <masakih@users.sourceforge.jp>
2020-05-10 11:33:28 +0900

    リバーシエンジンをViewControllerから切り離した

---
+++ Reversi/ReversiEngine.swift
@@ -0,0 +1,497 @@
+//
+//  ReversiEngine.swift
+//  Reversi
+//
+//  Created by Hori,Masaki on 2020/05/06.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+protocol BoardDelegate: AnyObject {
+    
+    func beginComputerThinking(_ reversi: ReversiEngine, turn: Disk)
+    func endComputerThinking(_ reversi: ReversiEngine, turn: Disk)
+    
+    func willPass(_ reversi: ReversiEngine, turn: Disk)
+}
+
+final class ReversiEngine {
+    
+    let boardView: BoardView
+    
+    weak var delegate: BoardDelegate?
+    
+    @Published private(set) var counts: (Int, Int) = (0, 0)
+    
+    @Published var blackPlayer: Player = .manual {
+        
+        didSet {
+            
+            if !isAnimating, currentPlayer == blackPlayer, case .computer = blackPlayer {
+                playTurnOfComputer()
+            }
+        }
+    }
+    @Published var whitePlayer: Player = .manual {
+           
+           didSet {
+               
+               if !isAnimating, currentPlayer == whitePlayer, case .computer = whitePlayer {
+                   playTurnOfComputer()
+               }
+           }
+       }
+    
+    /// どちらの色のプレイヤーのターンかを表します。ゲーム終了時は `nil` です。
+    @Published private(set) var turn: Disk? = .dark
+    
+    private var animationCanceller: Canceller?
+    private var isAnimating: Bool { animationCanceller != nil }
+    
+    private var playerCancellers: [Disk: Canceller] = [:]
+    
+    init(_ boardView: BoardView) {
+        
+        self.boardView = boardView
+        boardView.delegate = self
+    }
+    
+    var currentPlayer: Player? {
+        
+        switch turn {
+        case .dark?: return blackPlayer
+        case .light?: return whitePlayer
+        case .none: return nil
+        }
+    }
+}
+
+// MARK: Reversi logics
+extension ReversiEngine {
+    
+    /// New
+    private func updateCounts() {
+        
+        counts = (countDisks(of: .dark), countDisks(of: .light))
+    }
+    
+    /// `side` で指定された色のディスクが盤上に置かれている枚数を返します。
+    /// - Parameter side: 数えるディスクの色です。
+    /// - Returns: `side` で指定された色のディスクの、盤上の枚数です。
+    func countDisks(of side: Disk) -> Int {
+        var count = 0
+        
+        for y in boardView.yRange {
+            for x in boardView.xRange {
+                if boardView.diskAt(x: x, y: y) == side {
+                    count +=  1
+                }
+            }
+        }
+        
+        return count
+    }
+    
+    /// 盤上に置かれたディスクの枚数が多い方の色を返します。
+    /// 引き分けの場合は `nil` が返されます。
+    /// - Returns: 盤上に置かれたディスクの枚数が多い方の色です。引き分けの場合は `nil` を返します。
+    func sideWithMoreDisks() -> Disk? {
+        let darkCount = countDisks(of: .dark)
+        let lightCount = countDisks(of: .light)
+        if darkCount == lightCount {
+            return nil
+        } else {
+            return darkCount > lightCount ? .dark : .light
+        }
+    }
+    
+    private func flippedDiskCoordinatesByPlacingDisk(_ disk: Disk, atX x: Int, y: Int) -> [(Int, Int)] {
+        let directions = [
+            (x: -1, y: -1),
+            (x:  0, y: -1),
+            (x:  1, y: -1),
+            (x:  1, y:  0),
+            (x:  1, y:  1),
+            (x:  0, y:  1),
+            (x: -1, y:  0),
+            (x: -1, y:  1),
+        ]
+        
+        guard boardView.diskAt(x: x, y: y) == nil else {
+            return []
+        }
+        
+        var diskCoordinates: [(Int, Int)] = []
+        
+        for direction in directions {
+            var x = x
+            var y = y
+            
+            var diskCoordinatesInLine: [(Int, Int)] = []
+            flipping: while true {
+                x += direction.x
+                y += direction.y
+                
+                switch (disk, boardView.diskAt(x: x, y: y)) { // Uses tuples to make patterns exhaustive
+                case (.dark, .some(.dark)), (.light, .some(.light)):
+                    diskCoordinates.append(contentsOf: diskCoordinatesInLine)
+                    break flipping
+                case (.dark, .some(.light)), (.light, .some(.dark)):
+                    diskCoordinatesInLine.append((x, y))
+                case (_, .none):
+                    break flipping
+                }
+            }
+        }
+        
+        return diskCoordinates
+    }
+    
+    /// `x`, `y` で指定されたセルに、 `disk` が置けるかを調べます。
+    /// ディスクを置くためには、少なくとも 1 枚のディスクをひっくり返せる必要があります。
+    /// - Parameter x: セルの列です。
+    /// - Parameter y: セルの行です。
+    /// - Returns: 指定されたセルに `disk` を置ける場合は `true` を、置けない場合は `false` を返します。
+    func canPlaceDisk(_ disk: Disk, atX x: Int, y: Int) -> Bool {
+        !flippedDiskCoordinatesByPlacingDisk(disk, atX: x, y: y).isEmpty
+    }
+    
+    /// `side` で指定された色のディスクを置ける盤上のセルの座標をすべて返します。
+    /// - Returns: `side` で指定された色のディスクを置ける盤上のすべてのセルの座標の配列です。
+    func validMoves(for side: Disk) -> [(x: Int, y: Int)] {
+        var coordinates: [(Int, Int)] = []
+        
+        for y in boardView.yRange {
+            for x in boardView.xRange {
+                if canPlaceDisk(side, atX: x, y: y) {
+                    coordinates.append((x, y))
+                }
+            }
+        }
+        
+        return coordinates
+    }
+    
+    /// `x`, `y` で指定されたセルに `disk` を置きます。
+    /// - Parameter x: セルの列です。
+    /// - Parameter y: セルの行です。
+    /// - Parameter isAnimated: ディスクを置いたりひっくり返したりするアニメーションを表示するかどうかを指定します。
+    /// - Parameter completion: アニメーション完了時に実行されるクロージャです。
+    ///     このクロージャは値を返さず、アニメーションが完了したかを示す真偽値を受け取ります。
+    ///     もし `animated` が `false` の場合、このクロージャは次の run loop サイクルの初めに実行されます。
+    /// - Throws: もし `disk` を `x`, `y` で指定されるセルに置けない場合、 `DiskPlacementError` を `throw` します。
+    func placeDisk(_ disk: Disk, atX x: Int, y: Int, animated isAnimated: Bool, completion: ((Bool) -> Void)? = nil) throws {
+        let diskCoordinates = flippedDiskCoordinatesByPlacingDisk(disk, atX: x, y: y)
+        if diskCoordinates.isEmpty {
+            throw DiskPlacementError(disk: disk, x: x, y: y)
+        }
+        
+        if isAnimated {
+            let cleanUp: () -> Void = { [weak self] in
+                self?.animationCanceller = nil
+            }
+            animationCanceller = Canceller(cleanUp)
+            animateSettingDisks(at: [(x, y)] + diskCoordinates, to: disk) { [weak self] isFinished in
+                guard let self = self else { return }
+                guard let canceller = self.animationCanceller else { return }
+                if canceller.isCancelled { return }
+                cleanUp()
+
+                completion?(isFinished)
+                try? self.saveGame()
+                self.updateCounts()
+            }
+        } else {
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.boardView.setDisk(disk, atX: x, y: y, animated: false)
+                for (x, y) in diskCoordinates {
+                    self.boardView.setDisk(disk, atX: x, y: y, animated: false)
+                }
+                completion?(true)
+                try? self.saveGame()
+                self.updateCounts()
+            }
+        }
+    }
+    
+    /// `coordinates` で指定されたセルに、アニメーションしながら順番に `disk` を置く。
+    /// `coordinates` から先頭の座標を取得してそのセルに `disk` を置き、
+    /// 残りの座標についてこのメソッドを再帰呼び出しすることで処理が行われる。
+    /// すべてのセルに `disk` が置けたら `completion` ハンドラーが呼び出される。
+    private func animateSettingDisks<C: Collection>(at coordinates: C, to disk: Disk, completion: @escaping (Bool) -> Void)
+        where C.Element == (Int, Int)
+    {
+        guard let (x, y) = coordinates.first else {
+            completion(true)
+            return
+        }
+        
+        let animationCanceller = self.animationCanceller!
+        boardView.setDisk(disk, atX: x, y: y, animated: true) { [weak self] isFinished in
+            guard let self = self else { return }
+            if animationCanceller.isCancelled { return }
+            if isFinished {
+                self.animateSettingDisks(at: coordinates.dropFirst(), to: disk, completion: completion)
+            } else {
+                for (x, y) in coordinates {
+                    self.boardView.setDisk(disk, atX: x, y: y, animated: false)
+                }
+                completion(false)
+            }
+        }
+    }
+}
+
+// MARK: Game management
+
+extension ReversiEngine {
+    /// ゲームの状態を初期化し、新しいゲームを開始します。
+    func newGame() {
+        boardView.reset()
+        turn = .dark
+        blackPlayer = .manual
+        whitePlayer = .manual
+        
+        updateCounts()
+        
+        try? saveGame()
+    }
+    
+    /// プレイヤーの行動を待ちます。
+    func waitForPlayer() {
+        guard let player = self.currentPlayer else { return }
+        switch player {
+        case .manual:
+            break
+        case .computer:
+            playTurnOfComputer()
+        }
+    }
+    
+    /// プレイヤーの行動後、そのプレイヤーのターンを終了して次のターンを開始します。
+    /// もし、次のプレイヤーに有効な手が存在しない場合、パスとなります。
+    /// 両プレイヤーに有効な手がない場合、ゲームの勝敗を表示します。
+    func nextTurn() {
+        guard var turn = self.turn else { return }
+
+        turn.flip()
+        
+        if validMoves(for: turn).isEmpty {
+            if validMoves(for: turn.flipped).isEmpty {
+                
+                self.turn = nil
+                
+            } else {
+                
+                delegate?.willPass(self, turn: turn)
+                
+                self.turn = turn
+            }
+            
+        } else {
+            
+            self.turn = turn
+            waitForPlayer()
+        }
+    }
+    
+    /// "Computer" が選択されている場合のプレイヤーの行動を決定します。
+    func playTurnOfComputer() {
+        
+        guard let turn = self.turn else { preconditionFailure() }
+        let (x, y) = validMoves(for: turn).randomElement()!
+
+        delegate?.beginComputerThinking(self, turn: turn)
+        
+        let cleanUp: () -> Void = { [weak self] in
+            guard let self = self else { return }
+            self.playerCancellers[turn] = nil
+            
+            self.delegate?.endComputerThinking(self, turn: turn)
+        }
+        let canceller = Canceller(cleanUp)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            
+            guard let self = self else { return }
+            if canceller.isCancelled { return }
+            cleanUp()
+            
+            try! self.placeDisk(turn, atX: x, y: y, animated: true) { [weak self] _ in
+                self?.nextTurn()
+            }
+        }
+        
+        playerCancellers[turn] = canceller
+    }
+}
+
+
+
+extension ReversiEngine: BoardViewDelegate {
+    /// `boardView` の `x`, `y` で指定されるセルがタップされたときに呼ばれます。
+    /// - Parameter boardView: セルをタップされた `BoardView` インスタンスです。
+    /// - Parameter x: セルの列です。
+    /// - Parameter y: セルの行です。
+    func boardView(_ boardView: BoardView, didSelectCellAtX x: Int, y: Int) {
+        guard let turn = turn else { return }
+        if isAnimating { return }
+        guard case .manual = currentPlayer else { return }
+        // try? because doing nothing when an error occurs
+        try? placeDisk(turn, atX: x, y: y, animated: true) { [weak self] _ in
+            self?.nextTurn()
+        }
+    }
+}
+
+
+// MARK: Save and Load
+
+extension ReversiEngine {
+    private var path: String {
+        (NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first! as NSString).appendingPathComponent("Game")
+    }
+    
+    /// ゲームの状態をファイルに書き出し、保存します。
+    func saveGame() throws {
+        var output: String = ""
+        output += turn.symbol
+        output += blackPlayer.rawValue.description
+        output += whitePlayer.rawValue.description
+        output += "\n"
+        
+        for y in boardView.yRange {
+            for x in boardView.xRange {
+                output += boardView.diskAt(x: x, y: y).symbol
+            }
+            output += "\n"
+        }
+        
+        do {
+            try output.write(toFile: path, atomically: true, encoding: .utf8)
+        } catch let error {
+            throw FileIOError.read(path: path, cause: error)
+        }
+    }
+    
+    /// ゲームの状態をファイルから読み込み、復元します。
+    func loadGame() throws {
+        let input = try String(contentsOfFile: path, encoding: .utf8)
+        var lines: ArraySlice<Substring> = input.split(separator: "\n")[...]
+        
+        guard var line = lines.popFirst() else {
+            throw FileIOError.read(path: path, cause: nil)
+        }
+        
+        do { // turn
+            guard
+                let diskSymbol = line.popFirst(),
+                let disk = Optional<Disk>(symbol: diskSymbol.description)
+            else {
+                throw FileIOError.read(path: path, cause: nil)
+            }
+            turn = disk
+        }
+
+        // players
+        guard
+            let blackPlayerSymbol = line.popFirst(),
+            let blackPlayerNumber = Int(blackPlayerSymbol.description),
+            let whitePlayerSymbol = line.popFirst(),
+            let whitePlayerNumber = Int(whitePlayerSymbol.description)
+            else {
+                throw FileIOError.read(path: path, cause: nil)
+        }
+        blackPlayer = Player(rawValue: blackPlayerNumber) ?? .manual
+        whitePlayer = Player(rawValue: whitePlayerNumber) ?? .manual
+
+        do { // board
+            guard lines.count == boardView.height else {
+                throw FileIOError.read(path: path, cause: nil)
+            }
+            
+            var y = 0
+            while let line = lines.popFirst() {
+                var x = 0
+                for character in line {
+                    let disk = Disk?(symbol: "\(character)").flatMap { $0 }
+                    boardView.setDisk(disk, atX: x, y: y, animated: false)
+                    x += 1
+                }
+                guard x == boardView.width else {
+                    throw FileIOError.read(path: path, cause: nil)
+                }
+                y += 1
+            }
+            guard y == boardView.height else {
+                throw FileIOError.read(path: path, cause: nil)
+            }
+        }
+        
+        updateCounts()
+    }
+    
+    enum FileIOError: Error {
+        case write(path: String, cause: Error?)
+        case read(path: String, cause: Error?)
+    }
+}
+
+// MARK: Additional types
+
+extension ReversiEngine {
+    enum Player: Int {
+        case manual = 0
+        case computer = 1
+    }
+}
+
+final class Canceller {
+    private(set) var isCancelled: Bool = false
+    private let body: (() -> Void)?
+    
+    init(_ body: (() -> Void)?) {
+        self.body = body
+    }
+    
+    func cancel() {
+        if isCancelled { return }
+        isCancelled = true
+        body?()
+    }
+}
+
+struct DiskPlacementError: Error {
+    let disk: Disk
+    let x: Int
+    let y: Int
+}
+
+
+extension Optional where Wrapped == Disk {
+    fileprivate init?<S: StringProtocol>(symbol: S) {
+        switch symbol {
+        case "x":
+            self = .some(.dark)
+        case "o":
+            self = .some(.light)
+        case "-":
+            self = .none
+        default:
+            return nil
+        }
+    }
+    
+    fileprivate var symbol: String {
+        switch self {
+        case .some(.dark):
+            return "x"
+        case .some(.light):
+            return "o"
+        case .none:
+            return "-"
+        }
+    }
+}
